4592fb4c73d45d69c0271c204c39ca15592451e8
Vitor Reis <vitorcamargoreis@outlook.com.br>
2024-08-14 09:27:20 -0300

    simplificando tudo, docker desktop + K8S

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-create:
+create_aks:
 	@cd ./terraform && terraform apply --auto-approve
+
+create_kind:
+	@kind create cluster --config config.yaml
+
+metallb: 
+	@kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
+	@kubectl wait --namespace metallb-system  --for=condition=ready pod --selector=app=metallb --timeout=90s
 
 helm:
 	@helmfile apply 
--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "home-lab-aks" {
   name     = "home-lab-aks"
   location = "east us"
-}
+ }
 
 resource "azurerm_kubernetes_cluster" "home-lab-aks" {
   name                = "home-lab-aks"
@@ -38,7 +38,88 @@ resource "azurerm_kubernetes_cluster" "home-lab-aks" {
 }
 
 
-resource "azurerm_dns_zone" "home-lab-aks-public" {
-  name                = "vitorreis.dev.br"
-  resource_group_name = azurerm_resource_group.home-lab-aks.name
-}
+# # resource "azurerm_virtual_network" "example" {
+# #   name                = "network-home-lab"
+# #   location            = azurerm_resource_group.home-lab-aks.location
+# #   resource_group_name = azurerm_resource_group.home-lab-aks.name
+# #   address_space       = ["10.254.0.0/16"]
+# # }
+
+# # resource "azurerm_subnet" "example" {
+# #   name                 = "subnet-appgw"
+# #   virtual_network_name = azurerm_virtual_network.example.name
+# #   resource_group_name = azurerm_resource_group.home-lab-aks.name
+# #   address_prefixes     = ["10.254.0.0/24"]
+# # }
+
+# # resource "azurerm_public_ip" "example" {
+# #   name                = "pip-appgw"
+# #   sku                 = "Standard"
+# #   location            = azurerm_resource_group.home-lab-aks.location
+# #   resource_group_name = azurerm_resource_group.home-lab-aks.name
+# #   allocation_method   = "Static"
+# # }
+
+# # resource "azurerm_application_gateway" "network" {
+# #   name                = "appgateway"
+# #   location            = azurerm_resource_group.home-lab-aks.location
+# #   resource_group_name = azurerm_resource_group.home-lab-aks.name
+
+
+# #   sku {
+# #     name     = "WAF_v2"
+# #     tier     = "WAF_v2"
+# #     capacity = 2
+# #   }
+
+# #   gateway_ip_configuration {
+# #     name      = "my-gateway-ip-configuration"
+# #     subnet_id = azurerm_subnet.example.id
+# #   }
+
+# #   frontend_port {
+# #     name = "frontend_port_name"
+# #     port = 80
+# #   }
+
+# #   frontend_ip_configuration {
+# #     name                 = "frontend_ip_configuration_name"
+# #     public_ip_address_id = azurerm_public_ip.example.id
+# #   }
+
+# #   backend_address_pool {
+# #   name = "backend_address_pool_name"
+# #     ip_addresses = ["135.237.50.218"]
+# #   }
+
+# #   backend_http_settings {
+# #     name                  = "http_setting_name"
+# #     cookie_based_affinity = "Disabled"
+# #     path                  = ""
+# #     port                  = 80
+# #     protocol              = "Http"
+# #     request_timeout       = 60
+# #   }
+
+# #   http_listener {
+# #     name                           = "listener_name"
+# #     frontend_ip_configuration_name = "frontend_ip_configuration_name"
+# #     frontend_port_name             = "frontend_port_name"
+# #     protocol                       = "Http"
+# #   }
+
+# #   request_routing_rule {
+# #     name                       = "request_routing_rule_name"
+# #     priority                   = 9
+# #     rule_type                  = "Basic"
+# #     http_listener_name         = "listener_name"
+# #     backend_address_pool_name  = "backend_address_pool_name"
+# #     backend_http_settings_name = "http_setting_name"
+# #   }
+
+#   waf_configuration  {
+#   enabled          = false
+#   firewall_mode    = "Prevention"
+#   rule_set_version = "3.2"
+#   }
+# }
